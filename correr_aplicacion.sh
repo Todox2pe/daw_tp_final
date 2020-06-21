8f9db06a6a46e5d7874030aaf934c5e65756c496
Todox2pe <manuelalejandrocamino@gmail.com>
2020-06-20 21:51:09 -0700

    agregada verificacion previa a docker-compose up en caso de existencia db/data

--- correr_aplicacion.sh
+++ correr_aplicacion.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [ ! -d ./db/data ] ; then
-docker stop mysql-server
+docker-compose down
 ./start_mysql.sh mysql-net "$PWD"/db
 sleep 1
 docker stop mysql-server
 docker-compose up
 else
+docker-compose down
 docker-compose up
 fi
