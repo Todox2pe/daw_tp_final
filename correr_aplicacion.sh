8bfb66acaaa4b4e4e3d4f9737f1e62cfffeda1bb
Todox2pe <manuelalejandrocamino@gmail.com>
2020-06-20 21:59:03 -0700

    mejorada verificacion previo a correr aplicacion

--- correr_aplicacion.sh
+++ correr_aplicacion.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 if [ ! -d ./db/data ] ; then
+docker stop mysql-server
+docker rm mysql-server
+docker stop phpadmin
+docker rm phpadmin
+docker stop nodejs-container
+docker rm nodejs-container
 docker-compose down
 ./start_mysql.sh mysql-net "$PWD"/db
 sleep 1
